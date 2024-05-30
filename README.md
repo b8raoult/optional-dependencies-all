62000b6e50fa06256df4c60e1ddefc950f372aaa
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-05-30 19:08:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# optional-dependencies-all pre-commit hook
+
+A pre-commit hook to updates the `project.optional-dependencies.all` in a `pyproject.toml` file.
+
+The following code is inconsistent because `pip install mypackage[all]` will not install `requests`.
+This pre-commit hook will make sure that `all` list all the dependencies listed in other entries.
+You can also exclude some entries (typically `docs` or `dev`) with the `--exclude-keys` option.
+
+```toml
+
+[project]
+
+dependencies = [
+  "tqdm"
+]
+
+optional-dependencies.web = [
+  "requests",
+]
+
+optional-dependencies.all = [
+  "tqdm",
+]
+```
+
+becomes:
+
+```toml
+
+[project]
+
+dependencies = [
+  "tqdm"
+]
+
+optional-dependencies.web = [
+  "requests",
+]
+
+optional-dependencies.all = [
+  "tqdm",
+  "requests",
+]
+```
+
+Usage:
+
+```yaml
+- repo: https://github.com/b8raoult/optional-dependencies-all
+  rev: "0.1.4"
+  hooks:
+  - id: optional-dependencies-all
+    args: ["--all-key", "all", "--ignore-keys", "dev,docs"]
+```
---
+++ src/optional_dependencies_all/__main__.py
@@ -0,0 +1,85 @@
+import argparse
+import re
+import sys
+
+import toml
+
+
+def fix(path, inplace=False, all_key="all", exclude_keys=None, indent=2):
+    with open(path) as file:
+        text = file.read()
+
+    toml_dict = toml.loads(text)
+
+    project = toml_dict.get("project", {})
+    dependencies = project.get("dependencies", [])
+    optional_dependencies = project.get("optional-dependencies", {})
+
+    all_key = "all"
+    exclude_keys = ["dev", "docs"]
+    all_dependencies = set(dependencies)
+
+    if all_key not in optional_dependencies:
+        return
+
+    for key in optional_dependencies:
+        if key == all_key:
+            continue
+        if key in exclude_keys:
+            continue
+        all_dependencies.update(optional_dependencies[key])
+
+    if all_key in optional_dependencies:
+        optional_dependencies[all_key] = sorted(all_dependencies)
+
+    if inplace:
+        out = open(path, "w")
+    else:
+        out = sys.stdout
+
+    indent = " " * indent
+
+    lines = iter(text.splitlines())
+    for line in lines:
+        m = re.match(rf"^optional-dependencies.{all_key}\s*=\s*\[", line)
+        if m:
+            print(f"optional-dependencies.{all_key} = [", file=out)
+            for p in optional_dependencies.get(all_key, []):
+                print(f'{indent}"{p}",', file=out)
+            print("]", file=out)
+            while "]" not in line:
+                line = next(lines)
+        else:
+            print(line, file=out)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inplace", action="store_true", help="Modify the files in place."
+    )
+    parser.add_argument(
+        "--all-key", default="all", help="The key to use for the `all` dependencies."
+    )
+    parser.add_argument("--exclude-keys", help="The keys to exclude.")
+    parser.add_argument(
+        "--indent",
+        type=int,
+        default=2,
+        help="The number of spaces to use for indentation.",
+    )
+    parser.add_argument("files", nargs="+", help="The files to convert.")
+    args = parser.parse_args()
+
+    for path in args.files:
+        fix(
+            path,
+            inplace=args.inplace,
+            all_key=args.all_key,
+            exclude_keys=args.exclude_keys,
+            indent=args.indent,
+        )
+
+
+if __name__ == "__main__":
+    main()
